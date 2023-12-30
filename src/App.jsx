fff3fc797137376da1c53165447c1675be9b0e43
ugurhmz <craxx67@gmail.com>
2023-12-30 15:35:43 +0300

    Pages, Components and reac-router-dom addeed then shown Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
 import React, { useEffect, useState } from 'react'
 import {fetchDataFromApi}  from './utils/service'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getApiConfiguration } from './redux/homeSlice'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+//
+import Home from './pages/home/Home'
+import Details from './pages/details/Details'
+import PageNotFound from './pages/404/404'
+import Explore from './pages/explore/Explore'
+import SearchResult from './pages/searchResult/SearchResult'
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+
 
 
 const App = () => {
   const dispatch = useDispatch()
+  const {payloadObject} = useSelector(state => state.home)
+ 
+  console.log("xxx",payloadObject)
 
   useEffect(() => {
     getPopular()
@@ -15,14 +29,16 @@ const App = () => {
   const getPopular = () => {
      fetchDataFromApi("/movie/popular")
       .then((res) => {
-        console.log(res)
         dispatch(getApiConfiguration(res))
       })
   }
 
   return (
-    <div style={{backgroundColor:"white"}}>  
-     </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
