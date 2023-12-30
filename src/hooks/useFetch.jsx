d929b7493001d14b4e0af2e4df3aa46a20a2e632
ugurhmz <craxx67@gmail.com>
2023-12-30 16:46:12 +0300

    custom hooks added, useFetch

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { fetchDataFromApi } from '../utils/service'
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(null)
+    const [error, setError] = useState(null) 
+
+    useEffect(() => {
+        setLoading("loading...")
+        setData(null)
+        setError(null)
+
+        fetchDataFromApi(url)
+            .then( (res) => {
+                setLoading(false)
+                setData(res)
+            })
+            .catch((err) => {
+                console.log("ERR ~ useFetch: " , err)
+                setLoading(false)
+                setError("Something went wrong!")
+            })
+    }, [url])
+
+    return { data, loading, error} // state'leri dışarı dön.
+}
+
+export default useFetch
