9195d2e01c9eadd0b4500b2348732c2f3c781713
ugurhmz <craxx67@gmail.com>
2023-12-31 14:46:29 +0300

    Trending sec init

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import "./style.scss"
 
 import HeroBanner from './heroBanner/HeroBanner'
+import Trending from './trending/Trending'
+
 
 const Home = () => {
   return (
     <div className='homePage'>
         <HeroBanner />
+        <Trending/>
         <div style={{height: '500px'}}></div>
     </div>
   )
