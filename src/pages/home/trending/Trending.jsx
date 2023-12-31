3552a46b25580ada0783d61f79da1dc7bc99bc23
ugurhmz <craxx67@gmail.com>
2023-12-31 15:10:26 +0300

    Switch tabs for Trendings added

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 import ContentWrapper from '../../../components/contentwrapper/ContentWrapper'
 import './style.scss'
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs'
+
+
 const Trending = () => {
+
+
+  const tabChanged = (tab) => {
+
+  }
+
   return (
     <div className="carouselSection">
         <ContentWrapper>
             <span className="carouselTitle">Trending</span>
+            <SwitchTabs data={["Day", "Week"]} tabChanged={tabChanged}/>
         </ContentWrapper>
     </div>
   )
