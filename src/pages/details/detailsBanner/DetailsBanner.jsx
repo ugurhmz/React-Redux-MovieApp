f8c40c1a1dce354b0e9fe825d3abf852eb3ba44d
ugurhmz <craxx67@gmail.com>
2024-01-04 19:15:20 +0300

    Details page, background image added with opacity section

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
 
 import ContentWrapper from '../../../components/contentwrapper/ContentWrapper'
+import { useParams } from 'react-router-dom'
+import useFetch from '../../../hooks/useFetch'
+import { useSelector } from 'react-redux'
+import Img from '../../../components/lazyLoadImage/Img'
+import './style.scss'
 
 const DetailsBanner = ({ video, crew }) => {
 
+    const { mediaType, id } = useParams()
+    const {data, loading } = useFetch(`/${mediaType}/${id}`)
+    const { payloadObject } = useSelector( (state) => state.home)
+
+    console.log("payloadObject", payloadObject)
+    console.log("Banner data", data)
     const toHoursAndMinutes = (totalMinutes) => {
         const hours = Math.floor(totalMinutes / 60);
         const minutes = totalMinutes % 60;
         return `${hours}h${minutes > 0 ? ` ${minutes}m` : "" }`
     }
 
-
   return (
     <div className='detailsBanner'>
         { !loading ? (
-            <div>Details content</div>
+           <>
+            { !!data && (
+                <React.Fragment>
+                    <div className="backdrop-img">
+                        <Img src={payloadObject.backdrop + data.backdrop_path} />
+                        <div className='opacity-layer'></div>
+                    </div>
+                </React.Fragment>
+            )
+            }
+           </>
         ) : 
             (
                 <div className='detailsBannerSkeleton'>
