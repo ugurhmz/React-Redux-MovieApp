ab91fe912c0e64db6f172d212f657724898cb874
ugurhmz <craxx67@gmail.com>
2024-01-08 10:04:11 +0300

    Details Videos Section added

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -3,6 +3,9 @@ import DetailsBanner from './detailsBanner/DetailsBanner'
 import { useParams } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
 import Cast from '../cast/Cast'
+import VideosSection from '../videoSection/VideosSection'
+
+
 
 const Details = () => {
 
@@ -15,6 +18,7 @@ const Details = () => {
     <div>
       <DetailsBanner video={data?.results?.[0]}  crew={credits?.crew}/>
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading}/>
     </div>
   )
 }
--- src/pages/videoSection/VideosSection.jsx
+++ src/pages/videoSection/VideosSection.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import VideoPopup from '../../components/videoPopup/VideoPopup'
+import ContentWrapper from '../../components/contentwrapper/ContentWrapper'
+import './style.scss'
+import Img from '../../components/lazyLoadImage/Img'
+import PlayIcon from '../details/PlayBtn'
 
-const VideosSection = () => {
+const VideosSection = ({ data, loading }) => {
+    const [ show, setShow] = useState(false)
+    const [videoId, setVideoId] = useState(null)
+
+
+    const loadingSkeleton = () => {
+        return (
+            <div className="skItem">
+                <div className="thumb skeleton"></div>
+                <div className="row skeleton"></div>
+                <div className="row2 skeleton"></div>
+            </div>
+        )
+    }
+
   return (
-    <div>VideosSection</div>
+    <div className='videosSection'>
+        <ContentWrapper>
+            <div className="sectionHeading">Official Videos</div>
+            { !loading ? 
+              (
+                <div className='videos'>
+                    { data?.results?.map( (video) => (
+                        <div 
+                        key={video.id} 
+                        className='videoItem'
+                        onClick={() => {
+                            setVideoId(video.key)
+                            setShow(true)
+                        }}>
+                            <div className="videoThumbnail">
+                                <Img src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`}/>
+                                <PlayIcon/>
+                            </div>
+                            <div className="videoTitle">
+                                {video.name}
+                            </div>
+                        </div>
+                    ))
+
+                    }
+                </div>
+              ) :
+              (
+                <div className='videoSkeleton'>
+                    {loadingSkeleton()}
+                    {loadingSkeleton()}
+                    {loadingSkeleton()}
+                    {loadingSkeleton()}
+                </div>
+              )
+            }
+
+        </ContentWrapper>
+        <VideoPopup
+           show={show}
+            setShow={setShow}
+            videoId={videoId}
+            setVideoId={setVideoId}
+        />
+    </div>
   )
 }
 
