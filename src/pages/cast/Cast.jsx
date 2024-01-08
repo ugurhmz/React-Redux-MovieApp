d7f40d05e00f99834b0c60b244241435446ba5e0
ugurhmz <craxx67@gmail.com>
2024-01-08 09:38:17 +0300

    Details Cast section added

---
+++ src/pages/cast/Cast.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import ContentWrapper from '../../components/contentwrapper/ContentWrapper'
+import { useSelector } from 'react-redux';
+import './style.scss'
+import Img from '../../components/lazyLoadImage/Img';
+
+const Cast = ({data, loading}) => {
+
+ const {payloadObject} = useSelector((state) => state.home)
+
+ const skeleton = () => {
+    return (
+        <div className="skItem">
+            <div className="circle skeleton"></div>
+            <div className="row skeleton"></div>
+            <div className="row2 skeleton"></div>
+        </div>
+    );
+ };
+
+  return (
+    <div className='castSection'>
+        <ContentWrapper>
+            <div className="sectionHeading">Top Cast</div>
+            {
+                !loading ?  (
+                    <div className="listItems">
+                        {
+                            data?.map( (item) => {
+                                let imgUrl = item.profile_path ? payloadObject.profile + item.profile_path : ""
+                                return (
+                                    <div  key={item.id}  className="listItem">
+                                        <div className="profileImg">
+                                            <Img src={imgUrl} />
+                                        </div>
+                                        <div className="name">
+                                            {item.name}
+                                        </div>
+                                        <div className="character">
+                                            {item.character}
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                    </div>
+                ) : (
+                    <div className="castSkeleton">
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                        {skeleton()}
+                    </div>
+                )
+            }
+        </ContentWrapper>
+    </div>
+  )
+}
+
+export default Cast
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import DetailsBanner from './detailsBanner/DetailsBanner'
 import { useParams } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
+import Cast from '../cast/Cast'
 
 const Details = () => {
 
@@ -13,6 +14,7 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]}  crew={credits?.crew}/>
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   )
 }
